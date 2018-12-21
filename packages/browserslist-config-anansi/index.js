c68f7a48f6631007f35c13c9501aacc67bb25971
Nathaniel Tucker <me@ntucker.me>
2018-12-21 14:32:11 -0800

    [browserlist][fix] Fix exporting based on env

--- packages/browserslist-config-anansi/index.js
+++ packages/browserslist-config-anansi/index.js
@@ -1,4 +1,9 @@
-module.exports = {
+const env =
+  process.env.NODE_ENV === 'development' ? 'development' : 'production';
+
+const config = {
   production: ['last 2 versions', 'not < 0.05%', 'not dead', 'not op_mini all'],
   development: ['last 1 Chrome versions', 'last 1 Firefox versions'],
 };
+
+module.export = config[env];
