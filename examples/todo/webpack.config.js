94b783d50d3add67d8f7159938fc27c234db4eea
Nathaniel Tucker <me@ntucker.me>
2019-01-13 12:02:27 -0800

    [examples] Tweak webpack config for todo example

--- examples/todo/webpack.config.js
+++ examples/todo/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, argv) => {
   const config = generateConfig(env, argv);
   if (config.optimization.splitChunks) {
     config.optimization.splitChunks.cacheGroups.router = {
-      test: /[\\/]node_modules[\\/](react-router|history|resolve-pathname|value-equal|path-to-regexp|scroll-behavior)[\\/]/,
+      test: /[\\/]node_modules[\\/](react-router|react-router-dom|history|resolve-pathname|value-equal|path-to-regexp|scroll-behavior|react-router-scroll-4)[\\/].*/,
       name: 'router',
       chunks: 'all',
     };
