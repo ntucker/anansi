35fe9e00cebc5a8535ee258b6a22767215b1799c
Nathaniel Tucker <me@ntucker.me>
2019-01-12 18:23:37 -0800

    [examples] Make lazy routing simpler

--- examples/found/src/routes.js
+++ examples/found/src/routes.js
@@ -1,13 +1,14 @@
 import React, { lazy } from 'react'
 import { hot } from 'react-hot-loader'
 import { hotRouteConfig, makeRouteConfig, Route } from 'found'
+import lazyPage from 'components/lazyPage'
 
 import App from './App'
 
 export default (
   <Route path="/" Component={App}>
-    <Route Component={lazy(() => import(/* webpackChunkName: "Home" */'./pages/Home'))} />
-    <Route path="people" Component={lazy(() => import(/* webpackChunkName: "People" */'./pages/People'))} />
-    <Route path="error" Component={lazy(() => import(/* webpackChunkName: "ErrorPage" */'./pages/Error'))} />
+    <Route Component={lazyPage('Home')} />
+    <Route path="people" Component={lazyPage('People')} />
+    <Route path="error" Component={lazyPage('Error')} />
   </Route>
 )
