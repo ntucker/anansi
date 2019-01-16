6fc6d6f7e05bb7694db858476d5f556627f2db86
Nathaniel Tucker <me@ntucker.me>
2019-01-16 14:12:57 -0800

    [examples] Make react-router example use polyfill

--- examples/found/webpack.config.js
+++ examples/found/webpack.config.js
@@ -1,8 +1,6 @@
 const { makeConfig } = require('@anansi/webpack-config');
 
 const options = {
-  //libraryInclude: /node_modules\/(@anansi\/)/,
-  //libraryExclude: /node_modules(?!\/(@anansi\/))/,
   basePath: 'src',
   buildDir: 'generated_assets/',
 };
--- examples/todo/src/index.js
+++ examples/todo/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { ScrollContext } from 'react-router-scroll-4';
+import loadPolyfills from '@anansi/polyfill';
 
 import ErrorBoundary from 'components/ErrorBoundary';
 import ErrorLoggerContext from 'lib/ErrorLoggerContext';
@@ -12,14 +13,19 @@ function shouldUpdateScroll(prevRouterProps, { history: { action } }) {
   return action !== 'REPLACE';
 }
 const history = createBrowserHistory();
-ReactDOM.createRoot(document.body).render(
-  <ErrorLoggerContext.Provider value={() => console.error('what what')}>
-    <ErrorBoundary>
-      <Router history={history}>
-        <ScrollContext shouldUpdateScroll={shouldUpdateScroll}>
-          <App />
-        </ScrollContext>
-      </Router>
-    </ErrorBoundary>
-  </ErrorLoggerContext.Provider>,
-);
+
+async function init() {
+  await loadPolyfills();
+  ReactDOM.createRoot(document.body).render(
+    <ErrorLoggerContext.Provider value={() => console.error('what what')}>
+      <ErrorBoundary>
+        <Router history={history}>
+          <ScrollContext shouldUpdateScroll={shouldUpdateScroll}>
+            <App />
+          </ScrollContext>
+        </Router>
+      </ErrorBoundary>
+    </ErrorLoggerContext.Provider>,
+  );
+}
+init();
