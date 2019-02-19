c9cf39febf4c984e9e78a4938fa215fb240b8d27
Nathaniel Tucker <me@ntucker.me>
2019-02-18 21:43:35 -0800

    [pkg] Update example pkgs

--- examples/typescript/src/index.tsx
+++ examples/typescript/src/index.tsx
@@ -5,7 +5,7 @@ import { Router } from 'react-router-dom';
 import { ScrollContext } from 'react-router-scroll-4';
 import loadPolyfills from '@anansi/polyfill';
 import { RouteChildrenProps } from 'react-router';
-import { RestProvider } from 'rest-hooks';
+import { RestProvider, NetworkThrottler } from 'rest-hooks';
 import 'style/main.scss';
 
 import ErrorBoundary from 'components/ErrorBoundary';
@@ -22,13 +22,13 @@ function shouldUpdateScroll(
   return action !== 'REPLACE';
 }
 const history = createBrowserHistory();
-
+const throttler = new NetworkThrottler();
 async function init() {
   await loadPolyfills();
   ReactDOM.unstable_createRoot(document.body).render(
     <ErrorLoggerContext.Provider value={() => console.error('what what')}>
         <ErrorBoundary>
-          <RestProvider>
+          <RestProvider throttler={throttler}>
             <Router history={history}>
               <ScrollContext shouldUpdateScroll={shouldUpdateScroll}>
                 <App />
