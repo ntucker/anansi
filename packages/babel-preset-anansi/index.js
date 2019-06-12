8e3ae4eda6066850a963c4f274352582e299a95a
Nathaniel Tucker <me@ntucker.me>
2019-06-11 18:10:17 -0700

    [babel][fix] Don't use babel/runtime esmodules if node target is set

--- packages/babel-preset-anansi/index.js
+++ packages/babel-preset-anansi/index.js
@@ -32,7 +32,8 @@ function buildPreset(api, options = {}) {
     // if supportsModules is undefined or true then assume it can handle es modules.
     : (options.modules || (supportsModules === false ? 'auto' : false));
   // false won't transform so it's like saying use ES6
-  const useESModules = modules === false || (modules === 'auto' ? 'auto' : undefined);
+  // anything targetting node < 12 will need to not use esmodules
+  const useESModules = !options.nodeTarget && (modules === false || (modules === 'auto' ? 'auto' : undefined));
 
   let absoluteRuntimePath = undefined;
   try {
