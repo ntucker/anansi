b5cdd1f19074a2ebc5ad4ab553b291e3ec4a3955
Nathaniel Tucker <me@ntucker.me>
2019-10-28 22:47:11 -0700

    [babel][enhance] Keep preset-env loose based on

--- packages/babel-preset-anansi/index.js
+++ packages/babel-preset-anansi/index.js
@@ -151,6 +151,7 @@ function buildPreset(api, options = {}) {
         modules,
         useBuiltIns: options.useBuiltIns,
         corejs: options.corejs,
+        loose: options.legacyDecorators,
       },
     ]);
   }
