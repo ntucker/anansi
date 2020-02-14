4f99e5a116c17a704d2ce2fbc6f5fbeb9f3d4ad1
Nathaniel Tucker <me@ntucker.me>
2020-02-14 15:51:52 -0800

    enhance: Remove rule overrides that were in type-aware preset

--- packages/eslint-plugin/typescript.js
+++ packages/eslint-plugin/typescript.js
@@ -12,11 +12,6 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/unbound-method': 'warn',
-        '@typescript-eslint/no-unnecessary-type-assertion': [
-          'error',
-          { typesToIgnore: ['any'] },
-        ]
       },
     },
   ]
