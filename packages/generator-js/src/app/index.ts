74c7e4ea4678abf55dc187133bd94e21f05c5d92
Nathaniel Tucker <me@ntucker.me>
2020-02-16 21:32:00 -0800

    enhance: Prettier template files on write (#27)

--- packages/generator-js/src/app/index.ts
+++ packages/generator-js/src/app/index.ts
@@ -1,11 +1,30 @@
 import chalk from 'chalk';
 import shelobsay from 'shelobsay';
+import prettier from 'gulp-prettier';
+import filter from 'gulp-filter';
 
 import ConfigureGenerator from './ConfigureGenerator';
 
 export { ConfigureGenerator };
 
 export default class extends ConfigureGenerator {
+  constructor(args: string | string[], options: {}) {
+    super(args, options);
+    const jsFilter = filter(
+      [
+        '**/*.js',
+        '**/*.ts',
+        '**/*.tsx',
+        '**/*.json',
+        '**/*.html',
+        '**/*.scss',
+        '**/*.css',
+      ],
+      { restore: true },
+    );
+    this.registerTransformStream([jsFilter, prettier(), jsFilter.restore]);
+  }
+
   async prompting() {
     this.log(
       shelobsay(
