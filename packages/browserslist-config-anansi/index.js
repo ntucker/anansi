315d206ace5ae316129d687a4868c096fa41be0f
Nathaniel Tucker <me@ntucker.me>
2020-04-20 22:46:04 -0700

    feat: Browserslist environments - Defaults: modern browsers - Legacy: Include IE 11 - Development: Last version only - Test: node
    BREAKING CHANGE: Requires browserslist >4.12 and node>8

--- packages/browserslist-config-anansi/index.js
+++ packages/browserslist-config-anansi/index.js
@@ -1,9 +1,19 @@
-const env =
-  process.env.NODE_ENV === 'development' ? 'development' : 'production';
-
-const config = {
-  production: ['last 2 versions', 'not < 0.05%', 'not dead', 'not op_mini all'],
-  development: ['last 1 Chrome versions', 'last 1 Firefox versions'],
-};
-
-module.exports = config[env];
+module.exports = {
+  defaults: [
+    "> 1%",
+    "Firefox ESR",
+    "not dead",
+    "not ie<12",
+    "not OperaMini all"
+  ],
+  legacy: [
+    "> 0.5%",
+    "Firefox ESR",
+    "not dead",
+    "not ie<11",
+    "not safari<12.1",
+    "not OperaMini all"
+  ],
+  development:  ['last 1 Chrome versions', 'last 1 Firefox versions'],
+  test: ['current node'],
+}
