3dce23ced8a1fe613f3cb2e384eeb470d694aa81
Nathaniel Tucker <me@ntucker.me>
2020-05-25 15:05:26 -0700

    enhance: Allow ts-expect-error and object

--- packages/eslint-plugin/typescript.js
+++ packages/eslint-plugin/typescript.js
@@ -12,6 +12,67 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/ban-ts-comment':  [
+          'error',
+          {
+            'ts-expect-error': false,
+            'ts-ignore': true,
+            'ts-nocheck': true,
+            'ts-check': false,
+          },
+        ],
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: false,
+            types: {
+              String: {
+                message: 'Use string instead',
+                fixWith: 'string',
+              },
+              Boolean: {
+                message: 'Use boolean instead',
+                fixWith: 'boolean',
+              },
+              Number: {
+                message: 'Use number instead',
+                fixWith: 'number',
+              },
+              Symbol: {
+                message: 'Use symbol instead',
+                fixWith: 'symbol',
+              },
+
+              Function: {
+                message: [
+                  'The `Function` type accepts any function-like value.',
+                  'It provides no type safety when calling the function, which can be a common source of bugs.',
+                  'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
+                  'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.',
+                ].join('\n'),
+                fixWith: '(...args: any) => any',
+              },
+
+              // object typing
+              Object: {
+                message: [
+                  'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
+                  '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                  '- If you want a type meaning "any value", you probably want `unknown` instead.',
+                ].join('\n'),
+                fixWith: 'object',
+              },
+              '{}': {
+                message: [
+                  '`{}` actually means "any non-nullish value".',
+                  '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                  '- If you want a type meaning "any value", you probably want `unknown` instead.',
+                ].join('\n'),
+                fixWith: 'Record<string, unknown>',
+              },
+            }
+          },
+        ]
       },
     },
   ]
