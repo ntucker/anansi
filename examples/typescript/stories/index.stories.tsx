8d1d572a1574be83fc02e923378d81f5e3972c1c
Nathaniel Tucker <me@ntucker.me>
2020-12-24 23:58:05 -0800

    enhance: Improve storybook support (#58)
    * enhance: Improve storybook support
    
    * better api usage
    
    Co-authored-by: Jordan Harband <ljharb@gmail.com>
    
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

--- examples/typescript/stories/index.stories.tsx
+++ examples/typescript/stories/index.stories.tsx
@@ -1,14 +1,11 @@
-import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 
 export default { title: 'atoms/Button' };
 
-export const text = () => (
-  <Button onClick={action('clicked')}>Hello Button</Button>
-);
+export const text = () => <Button>Hello Button</Button>;
 
 export const emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
