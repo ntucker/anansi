6a2e6438b34a15e7f88758c862ac0657b134e18f
Nathaniel Tucker <me@ntucker.me>
2021-01-25 01:19:21 -0600

    fix: Copy correct file for storybook gen

--- packages/generator-js/src/storybook/index.ts
+++ packages/generator-js/src/storybook/index.ts
@@ -26,8 +26,8 @@ class WebpackGenerator extends InstallPeersMixin(BetterGenerator) {
       this.config.getAll(),
     );
     this.fs.copyTpl(
-      this.templatePath('.storybook/preview.jsx'),
-      this.destinationPath('.storybook/preview.jsx'),
+      this.templatePath('.storybook/preview.tsx'),
+      this.destinationPath('.storybook/preview.tsx'),
       this.config.getAll(),
     );
     if (this.config.get('webpack')) {
