c526a576abc3ba1cc3f47cd490954077cd2e5161
Nathaniel Tucker <me@ntucker.me>
2021-03-30 12:18:49 -0500

    pkg: Storybook 6.2 final

--- packages/generator-js/src/storybook/index.ts
+++ packages/generator-js/src/storybook/index.ts
@@ -52,11 +52,11 @@ class WebpackGenerator extends InstallPeersMixin(BetterGenerator) {
   writingPkg() {
     const pkgJson = {
       devDependencies: {
-        '@storybook/addon-essentials': '^6.2.0-rc.6',
-        '@storybook/addon-links': '^6.2.0-rc.6',
-        '@storybook/addons': '^6.2.0-rc.6',
-        '@storybook/react': '^6.2.0-rc.6',
-        '@storybook/builder-webpack5': '^6.2.0-rc.6',
+        '@storybook/addon-essentials': '^6.2.0',
+        '@storybook/addon-links': '^6.2.0',
+        '@storybook/addons': '^6.2.0',
+        '@storybook/react': '^6.2.0',
+        '@storybook/builder-webpack5': '^6.2.0',
       },
     };
     this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
