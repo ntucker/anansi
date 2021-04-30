3b272399514d29c6c88e8377f5ee71a874dab477
Nathaniel Tucker <me@ntucker.me>
2021-04-30 10:25:26 -0500

    internal: Fix linting violations

--- examples/typescript/src/github/StaticBlock.js
+++ examples/typescript/src/github/StaticBlock.js
@@ -1,14 +1,13 @@
-import BaseResource from './BaseResource'
+import BaseResource from './BaseResource';
 
 class StaticResource extends BaseResource {
-  static a
-  static urlRoot = '/2/'
+  static a;
+  static urlRoot = '/2/';
   static {
-    this.a = this.urlRoot
+    this.a = this.urlRoot;
   }
-
 }
 
 export default class MyResource extends StaticResource {
-  static urlRoot = '/rootof/static/'
+  static urlRoot = '/rootof/static/';
 }
