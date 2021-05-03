b7968c4e54099dc3c7510a6930b7ab0536bc8130
Nathaniel Tucker <me@ntucker.me>
2021-05-02 20:01:58 -0500

    feat: Add types

---
+++ packages/webpack-config-anansi/index.d.ts
@@ -0,0 +1,51 @@
+import type {
+  Configuration,
+  RuleSetUseItem,
+  RuleSetConditionAbsolute,
+} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { OptimizeOptions } from 'svgo';
+import { StrictOptions } from '@linaria/babel-preset';
+import type { Options as TsconfigPathsOptions } from 'tsconfig-paths-webpack-plugin/lib/options';
+import type { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import type { TransformOptions } from '@babel/core';
+
+export interface Options {
+  rootPath?: string;
+  basePath?: string;
+  babelRoot?: string;
+  libraryInclude?: RuleSetConditionAbsolute;
+  libraryExclude?: RuleSetConditionAbsolute;
+  buildDir?: string;
+  serverDir?: string;
+  manifestFilename?: string;
+  mode?: Configuration['mode'];
+  nohash?: boolean;
+  argv?: any[];
+  env?: any;
+  htmlOptions?: HtmlWebpackPlugin.Options | false;
+  svgoOptions?: OptimizeOptions | false;
+  svgrOptions?: any | false;
+  linariaOptions?: StrictOptions | false;
+  tsconfigPathsOptions?: TsconfigPathsOptions | false;
+  globalStyleDir?: string | false;
+  sassResources?: string[];
+  cssModuleOptions?: any;
+  fontPreload?: 'preload' | 'prefetch';
+  bundleAnalyzerOptions?: BundleAnalyzerPlugin.Options;
+  babelLoader?: TransformOptions & {
+    cacheDirectory?: string;
+    cacheIdentifier?: any;
+    cacheCompression?: boolean;
+    customize?: any;
+    metadataSubscribers?: any[];
+  }; // babel loader options
+  extraJsLoaders?: RuleSetUseItem[];
+}
+
+export function makeConfig(
+  options: Options,
+): (env: any, argv: any[]) => Configuration;
+export function makeStorybookConfigGenerator(
+  baseConfig: Configuration,
+): ({ config: Configuration, mode: string }) => Configuration;
