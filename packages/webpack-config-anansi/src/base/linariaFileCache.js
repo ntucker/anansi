9cabb9c7458094fe880efc90c13726eba956c96d
Nathaniel Tucker <me@ntucker.me>
2022-01-03 22:05:52 -0600

    pkg: linaria to 3-beta.17

---
+++ packages/webpack-config-anansi/src/base/linariaFileCache.js
@@ -0,0 +1,42 @@
+const findCacheDir = require('find-cache-dir');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+const mkdirp = require('mkdirp');
+
+const hashFileName = name => {
+  const hash = crypto.createHash('md4');
+  hash.update(name);
+  return hash.digest('hex');
+};
+
+class LinariaFileCache {
+  constructor() {
+    const linariaCacheDir = findCacheDir({
+      name: `.linaria-${process.env.NODE_ENV}`,
+      cwd: process.cwd(),
+    });
+    if (!linariaCacheDir) {
+      throw new Error('Get linaria cache dir failed');
+    }
+    mkdirp.sync(linariaCacheDir);
+    this.linariaCacheDir = linariaCacheDir;
+  }
+
+  async get(key) {
+    return fs.promises.readFile(
+      path.join(this.linariaCacheDir, `${hashFileName(key)}.css`),
+      'utf8',
+    );
+  }
+
+  async set(key, value) {
+    return fs.promises.writeFile(
+      path.join(this.linariaCacheDir, `${hashFileName(key)}.css`),
+      value,
+      'utf8',
+    );
+  }
+}
+
+module.exports = new LinariaFileCache();
