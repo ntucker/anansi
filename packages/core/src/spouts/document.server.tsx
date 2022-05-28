c07e9a29935e0e24f815371ad6610bed77da967e
Nathaniel Tucker <me@ntucker.me>
2022-05-27 22:05:49 -0500

    enhance: Improve route asset identification

--- packages/core/src/spouts/document.server.tsx
+++ packages/core/src/spouts/document.server.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { Route } from '@anansi/router';
+import { StatsChunkGroup } from 'webpack';
 
 import type { ServerProps, ResolveProps } from './types';
 import Document from './DocumentComponent';
@@ -20,23 +21,45 @@ export default function DocumentSpout(options: {
     return async (props: ServerProps) => {
       const nextProps = await next(props);
 
-      const entrypoint = props.clientManifest?.entrypoints?.main;
       const publicPath = props.clientManifest.publicPath;
 
-      if (entrypoint === undefined || publicPath === undefined)
+      if (
+        Object.keys(props.clientManifest?.entrypoints ?? {}).length < 1 ||
+        publicPath === undefined
+      )
         throw new Error('Manifest missing entries needed');
 
       // TODO: consider using this package for build stats in future:
       // https://github.com/facebook/react/tree/main/packages/react-server-dom-webpack
       const assetMap = (assets: { name: string; size?: number }[]) =>
         assets.map(({ name }) => `${publicPath}${name}`);
+
+      const assetList: string[] = [];
+      Object.values(props.clientManifest?.entrypoints ?? {}).forEach(
+        entrypoint => {
+          assetList.push(...assetMap(entrypoint.assets ?? []));
+        },
+      );
+      new Set(
+        assetMap(
+          Object.values(props.clientManifest.namedChunkGroups ?? {})
+            .filter(({ name }) =>
+              nextProps.matchedRoutes.some(route => name?.includes(route.name)),
+            )
+            .flatMap(chunk => [
+              ...(chunk.assets ?? []),
+              // any chunk preloads
+              ...childrenAssets(chunk),
+            ]),
+        ),
+      ).forEach(asset => assetList.push(asset));
+
       // find additional assets to preload based on matched route
-      const assets = assetMap([
-        ...(entrypoint.assets ?? []),
-        ...(entrypoint.children?.prefetch?.find?.(({ name }) =>
-          nextProps.matchedRoutes.some(route => name === route.name),
-        )?.assets ?? []),
-      ]).map(asset =>
+      const assets: {
+        href: string;
+        as?: string | undefined;
+        rel?: string | undefined;
+      }[] = assetList.map(asset =>
         asset.endsWith('.css')
           ? { href: asset, rel: 'stylesheet' }
           : asset.endsWith('.js')
@@ -59,4 +82,12 @@ export default function DocumentSpout(options: {
       };
     };
   };
+}
+
+function childrenAssets(chunk: StatsChunkGroup) {
+  return chunk.children
+    ? Object.values(chunk.children).flatMap(preload =>
+        preload.flatMap(c => c.assets ?? []),
+      )
+    : [];
 }
