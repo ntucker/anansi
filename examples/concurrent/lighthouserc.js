5149651ddb27f22d1c2757f0d63e277a368ea161
Nathaniel Tucker <me@ntucker.me>
2022-05-28 21:23:15 -0500

    internal: Run lighthouse on react 18

---
+++ examples/concurrent/lighthouserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    collect: {
+      // collect options here
+      //staticDistDir: 'dist',
+      //isSinglePageApplication: true,
+      startServerCommand: 'yarn start:prod',
+      startServerReadyPattern: 'Listening at',
+      url: [
+        'http://localhost:8080',
+        'http://localhost:8080/user/6',
+        'http://localhost:8080/post/51',
+      ],
+    },
+    upload: {
+      // upload options here
+      target: 'temporary-public-storage',
+    },
+    server: {
+      // server options here
+    },
+    wizard: {
+      // wizard options here
+    },
+  },
+};
