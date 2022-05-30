37fb3f221a410f0c81b83a89f7452f9f0b7ca07b
Nathaniel Tucker <me@ntucker.me>
2022-05-30 13:26:01 -0500

    internal: Fix lightout config

--- examples/concurrent/lighthouserc.js
+++ examples/concurrent/lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       // collect options here
       //staticDistDir: 'dist',
       //isSinglePageApplication: true,
-      startServerCommand: 'yarn start:prod',
+      startServerCommand: 'yarn start:server',
       startServerReadyPattern: 'Listening at',
       url: [
         'http://localhost:8080',
