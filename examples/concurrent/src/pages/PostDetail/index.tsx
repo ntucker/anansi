63313c1bc7dc4b0196cb1a663b4da00a4c66aeec
Nathaniel Tucker <me@ntucker.me>
2022-06-16 19:52:52 -0500

    enhance: Add post edit

--- examples/concurrent/src/pages/PostDetail/index.tsx
+++ examples/concurrent/src/pages/PostDetail/index.tsx
@@ -1,20 +1,30 @@
-import { useSuspense } from 'rest-hooks';
-import { EditOutlined } from '@ant-design/icons';
-import { Button, Breadcrumb } from 'antd';
+import { useSuspense, useController } from 'rest-hooks';
+import { EditOutlined, SaveOutlined, LoadingOutlined } from '@ant-design/icons';
+import { Button, Input, Breadcrumb, Form } from 'antd';
 import { Img } from '@rest-hooks/img';
+import { useLoading } from '@rest-hooks/hooks';
 import { Card, Avatar } from 'antd';
 import { Link } from '@anansi/router';
 import { styled } from '@linaria/react';
-import { lazy } from 'react';
+import { lazy, useCallback, useState } from 'react';
+import { css } from '@linaria/core';
 
 import { PostResource, UserResource } from 'resources/Discuss';
 import Boundary from 'components/Boundary';
 
 export type Props = { id: string };
 const { Meta } = Card;
-
+const { TextArea } = Input;
 const Breading = styled(Breadcrumb)`
   margin: 16px 0;
+`;
+const editMeta = css`
+  > .ant-card-meta-detail {
+    width: 100%;
+  }
+  .ant-form-item {
+    margin-bottom: 0;
+  }
 `;
 
 const CommentList = lazy(
@@ -38,19 +48,7 @@ export default function PostDetail({ id }: Props) {
         </Breadcrumb.Item>
         <Breadcrumb.Item>{post.title}</Breadcrumb.Item>
       </Breading>
-      <Card
-        actions={[
-          <a href={`/post/${post.id}/edit`} key="edit">
-            <Button shape="circle" icon={<EditOutlined />} size="small" />
-          </a>,
-        ]}
-      >
-        <Meta
-          avatar={<Img component={Avatar} src={author?.profileImage} />}
-          title={post.title}
-          description={post.body}
-        />
-      </Card>
+      <PostMain post={post} author={author} />
       <Boundary fallback={<CardLoading />}>
         <CommentList postId={id} />
       </Boundary>
@@ -59,4 +57,78 @@ export default function PostDetail({ id }: Props) {
 }
 export function CardLoading() {
   return <Card style={{ marginTop: 16 }} loading={true} />;
+}
+
+function PostMain({
+  post,
+  author,
+}: {
+  post: PostResource;
+  author: UserResource;
+}) {
+  const { fetch } = useController();
+
+  const [edit, setEdit] = useState(false);
+
+  const toggleEdit = useCallback(() => {
+    if (edit) return;
+    setEdit(true);
+  }, [edit]);
+
+  const [onFinish, loading] = useLoading(
+    async (values: any) => {
+      if (edit)
+        await fetch(PostResource.partialUpdate(), { id: post.id }, values);
+      setEdit(edit => !edit);
+    },
+    [post.id, fetch, edit],
+  );
+  const content = edit ? (
+    <Meta
+      className={editMeta}
+      avatar={<Img component={Avatar} src={author?.profileImage} />}
+      title={
+        <Form.Item name="title">
+          <Input style={{ width: '100%' }} />
+        </Form.Item>
+      }
+      description={
+        <Form.Item name="body">
+          <TextArea rows={6} style={{ width: '100%' }} />
+        </Form.Item>
+      }
+    />
+  ) : (
+    <Meta
+      avatar={<Img component={Avatar} src={author?.profileImage} />}
+      title={post.title}
+      description={post.body}
+    />
+  );
+  return (
+    <Form onFinish={onFinish} initialValues={{ ...post }}>
+      <Card
+        actions={[
+          <Button
+            onClick={toggleEdit}
+            key={edit ? 'save' : 'edit'}
+            shape="circle"
+            icon={
+              loading ? (
+                <LoadingOutlined />
+              ) : edit ? (
+                <SaveOutlined />
+              ) : (
+                <EditOutlined />
+              )
+            }
+            size="small"
+            htmlType={edit ? 'submit' : 'button'}
+          />,
+        ]}
+      >
+        {content}
+      </Card>
+    </Form>
+  );
 }
