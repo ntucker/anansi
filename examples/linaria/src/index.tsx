acbec1394d7966f9635e3e70aa997d60ae40aa84
Nathaniel Tucker <me@ntucker.me>
2022-07-24 09:26:27 -0500

    internal: Remove polyfill package

--- examples/linaria/src/index.tsx
+++ examples/linaria/src/index.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import loadPolyfills from '@anansi/polyfill';
 
 import App from './App';
 
-async function init() {
-  await loadPolyfills();
-}
-init();
 ReactDOM.createRoot(document.body).render(<App />);
