097e7ed2beba4b0caf0eb391b7cd545d6be8cca1
Nathaniel Tucker <me@ntucker.me>
2022-11-18 16:08:04 -0600

    enhance: Fix menu deprecation

--- examples/typescript/src/navigation/Nav.tsx
+++ examples/typescript/src/navigation/Nav.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
+import { Menu, MenuProps } from 'antd';
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
@@ -9,26 +9,32 @@ function Nav() {
       mode="horizontal"
       defaultSelectedKeys={['1']}
       style={{ lineHeight: '64px' }}
-    >
-      <Menu.Item key="1">
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Link to="/posts">Posts</Link>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Link to="/issues">Issues</Link>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <Link to="/graphql">GraphQL</Link>
-      </Menu.Item>
-      <Menu.Item key="5">
-        <Link to="/slow">Slow</Link>
-      </Menu.Item>
-      <Menu.Item key="6">
-        <Link to="/error">Error</Link>
-      </Menu.Item>
-    </Menu>
+      items={items}
+    />
   );
 }
 export default memo(Nav);
+
+const items: MenuProps['items'] = [
+  getItem(<Link to="/">Home</Link>, 1),
+  getItem(<Link to="/posts">Posts</Link>, 2),
+  getItem(<Link to="/issues">Issues</Link>, 3),
+  getItem(<Link to="/graphql">GraphQL</Link>, 4),
+  getItem(<Link to="/slow">Slow</Link>, 5),
+  getItem(<Link to="/error">Error</Link>, 6),
+];
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuProps['items'],
+  type?: 'group',
+) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
