af4b618c57e7561927c8b4b2d8d77496903732b8
Nathaniel Tucker <me@ntucker.me>
2022-11-22 20:57:22 -0600

    enhance: useLive() in template

--- packages/generator-js/src/anansi-splash/templates/src/pages/Home/AssetPrice.tsx
+++ packages/generator-js/src/anansi-splash/templates/src/pages/Home/AssetPrice.tsx
@@ -1,4 +1,4 @@
-import { useSuspense, useSubscription } from '@rest-hooks/react';
+import { useLive } from '@rest-hooks/react';
 import { getExchangeRates } from 'api/ExchangeRates';
 
 export interface Props {
@@ -7,20 +7,18 @@ export interface Props {
 
 export default function AssetPrice({ symbol }: Props) {
   // Learn more about Rest Hooks: https://resthooks.io/docs/getting-started/usage
-  const { data: price } = useSuspense(getExchangeRates, {
+  const { data: price } = useLive(getExchangeRates, {
     currency: 'USD',
   });
-  // https://resthooks.io/docs/api/useSubscription
-  useSubscription(getExchangeRates, {
-    currency: 'USD',
-  });
-  const displayPrice = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(1 / Number.parseFloat(price.rates[symbol]));
+  const displayPrice = formatPrice.format(1 / Number.parseFloat(price.rates[symbol]));
   return (
     <span>
       {symbol} {displayPrice}
     </span>
   );
 }
+
+const formatPrice = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
