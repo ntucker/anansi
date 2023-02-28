d4091c69596ee4874532f3ca8f7601545a0f1adf
Nathaniel Tucker <me@ntucker.me>
2023-02-28 09:48:37 -0600

    enhance: Add svg to test to ensure working

--- examples/concurrent/src/pages/Home/index.tsx
+++ examples/concurrent/src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
 import { Typography } from 'antd';
 
+import AngleDownUrl, {
+  ReactComponent as AngleDown,
+} from './angle-down-solid.svg';
+
 const { Title } = Typography;
 
 export default function Home() {
-  return <Title>hello world</Title>;
+  return (
+    <Title>
+      hello world <img src={AngleDownUrl} style={{ width: '1em' }} />{' '}
+      <AngleDown style={{ width: '1em' }} />
+    </Title>
+  );
 }
