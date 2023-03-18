f5790325d664d7c3b0ee90e6eafe7923e1e998c1
Nathaniel Tucker <me@ntucker.me>
2023-03-17 22:24:02 -0500

    fix: Setup script works with native and node env

--- packages/jest-preset-anansi/src/scripts/testSetup.js
+++ packages/jest-preset-anansi/src/scripts/testSetup.js
@@ -1,16 +1,29 @@
-require('whatwg-fetch');
-require('core-js/stable');
-window.requestIdleCallback = jest.fn().mockImplementation(cb => {
-  cb();
-});
-class Worker {
-  constructor(stringUrl) {
-    this.url = stringUrl;
-    this.onmessage = () => {};
-  }
+// node env
+if (typeof window === 'undefined') {
+  require('cross-fetch/polyfill');
+  // reactnative env
+} else if (typeof document === 'undefined') {
+  import('node-fetch').then(({ default: fetch }) => {
+    // @ts-ignore
+    // eslint-disable-next-line no-undef
+    globalThis.fetch = fetch;
+  });
+  // dom env
+} else {
+  require('whatwg-fetch');
+  require('core-js/stable');
+  window.requestIdleCallback = jest.fn().mockImplementation(cb => {
+    cb();
+  });
+  class Worker {
+    constructor(stringUrl) {
+      this.url = stringUrl;
+      this.onmessage = () => {};
+    }
 
-  postMessage(msg) {
-    this.onmessage(msg);
+    postMessage(msg) {
+      this.onmessage(msg);
+    }
   }
+  window.Worker = Worker;
 }
-window.Worker = Worker;
