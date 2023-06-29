7a291a9cd29e2a8167e4124b413845cab0ecc7c9
Nathaniel Tucker <me@ntucker.me>
2023-06-29 09:41:32 -0500

    feat: Add useLocationSearch()

--- packages/pojo-router/src/hooks.tsx
+++ packages/pojo-router/src/hooks.tsx
@@ -6,6 +6,15 @@ export function useLocation() {
   return useContext(LocationContext);
 }
 
+export function useLocationSearch<K extends string | undefined = undefined>(
+  key?: K,
+): (K extends string ? string : URLSearchParams) | null {
+  const location = useLocation();
+  const search = new URLSearchParams(location?.search?.substring?.(1));
+  if (key) return search.get(key) as any;
+  return search as any;
+}
+
 export function useController() {
   return useContext(ControllerContext);
 }
