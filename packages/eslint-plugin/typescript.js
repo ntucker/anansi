61231979a144dc465589a1da9f04b588d1e471c8
Nathaniel Tucker <me@ntucker.me>
2024-08-24 10:15:06 +0200

    fix: Compatibility across typescript-eslint 7 and 8

--- packages/eslint-plugin/typescript.js
+++ packages/eslint-plugin/typescript.js
@@ -1,3 +1,88 @@
+const semver = require('semver');
+let tsEslintPkg;
+try {
+  tsEslintPkg = require('@typescript-eslint/eslint-plugin/package.json');
+  // eslint-disable-next-line no-empty
+} catch (e) {}
+
+let rules = {
+  'react/prop-types': 'off',
+  'react/jsx-uses-react': 'off',
+  'react/react-in-jsx-scope': 'off',
+  '@typescript-eslint/no-require-imports': 'off',
+  '@typescript-eslint/explicit-function-return-type': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/explicit-member-accessibility': 'off',
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
+  '@typescript-eslint/no-use-before-define': 'off',
+  '@typescript-eslint/no-unused-vars': [
+    'warn',
+    // ignoreRestSiblings is disabled by default
+    {
+      vars: 'all',
+      args: 'after-used',
+      ignoreRestSiblings: true,
+      argsIgnorePattern: '^_',
+    },
+  ],
+  '@typescript-eslint/ban-ts-comment': [
+    'error',
+    {
+      'ts-expect-error': false,
+      'ts-ignore': true,
+      'ts-nocheck': true,
+      'ts-check': false,
+    },
+  ],
+};
+
+if (!semver.gte(tsEslintPkg.version, '8.0.0')) {
+  rules['@typescript-eslint/ban-types'] = [
+    'error',
+    {
+      extendDefaults: false,
+      types: {
+        String: {
+          message: 'Use string instead',
+          fixWith: 'string',
+        },
+        Boolean: {
+          message: 'Use boolean instead',
+          fixWith: 'boolean',
+        },
+        Number: {
+          message: 'Use number instead',
+          fixWith: 'number',
+        },
+        Symbol: {
+          message: 'Use symbol instead',
+          fixWith: 'symbol',
+        },
+
+        Function: {
+          message: [
+            'The `Function` type accepts any function-like value.',
+            'It provides no type safety when calling the function, which can be a common source of bugs.',
+            'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
+            'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.',
+          ].join('\n'),
+          fixWith: '((...args: any) => any)',
+        },
+
+        // object typing
+        Object: {
+          message: [
+            'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
+            '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+            '- If you want a type meaning "any value", you probably want `unknown` instead.',
+          ].join('\n'),
+          fixWith: 'object',
+        },
+      },
+    },
+  ];
+}
+
 module.exports = {
   extends: ['./base.js'],
   overrides: [
@@ -9,36 +94,7 @@ module.exports = {
           typescript: {},
         },
       },
-      rules: {
-        'react/prop-types': 'off',
-        'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-require-imports': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-member-accessibility': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          // ignoreRestSiblings is disabled by default
-          {
-            vars: 'all',
-            args: 'after-used',
-            ignoreRestSiblings: true,
-            argsIgnorePattern: '^_',
-          },
-        ],
-        '@typescript-eslint/ban-ts-comment': [
-          'error',
-          {
-            'ts-expect-error': false,
-            'ts-ignore': true,
-            'ts-nocheck': true,
-            'ts-check': false,
-          },
-        ],
-      },
+      rules,
     },
   ],
 };
