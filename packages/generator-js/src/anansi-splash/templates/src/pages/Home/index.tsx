b101af08f22308dd62610513229158b961ad3009
Nathaniel Tucker <me@ntucker.me>
2024-09-06 15:05:40 +0200

    enhance: Use more linaria in splash init

--- packages/generator-js/src/anansi-splash/templates/src/pages/Home/index.tsx
+++ packages/generator-js/src/anansi-splash/templates/src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
 import AssetPrice from './AssetPrice';
+<% if (style === 'linaria') { %>
+import { styled } from '@linaria/react';
+
+const Head = styled.p`
+  font-size: 30px;
+`;
+<% } else { %>
+const Head = ({children}) => <p style={{ fontSize: '30px' }}>{children}</p>;
+<% } %>
 
 export default function Home() {
   return (
     <>
-      <p style={{ fontSize: '30px' }}>
+      <Head>
         Congrats! You&apos;ve created <%= appName %>!
-      </p>
+      </Head>
       <p style={{ fontSize: '15px' }}>
         Check out the generated ReadMe for instructions on how to use this
         library
