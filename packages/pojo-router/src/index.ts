bdd6fcf5542b638d4f985620efd34220ef1440c3
Nathaniel Tucker <me@ntucker.me>
2025-05-15 14:10:04 -0400

    fix: Export for ControllerProps

--- packages/pojo-router/src/index.ts
+++ packages/pojo-router/src/index.ts
@@ -1,10 +1,7 @@
 export * from './types.js';
 export { default as PojoRouter } from './PojoRouter.js';
-export {
-  default as RouteController,
-  default as Router,
-  type ControllerProps,
-} from './Controller.js';
+export { default as RouteController, default as Router } from './Controller.js';
+export type { ControllerProps } from './Controller.js';
 export * from './hooks.js';
 export { default as Link } from './Link.js';
 export * from './context.js';
